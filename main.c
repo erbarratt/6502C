24441ef0646c0dc73e757685a0d2acc6f0779407
Elliott Barratt <elbazuk@gmail.com>
2021-03-29 14:04:50 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,19 @@
+/*
+	Original code in C++ written by onelonecoder / onelonecoder.com
+	
+	https://github.com/OneLoneCoder/olcNES/tree/master/Part%232%20-%20CPU
+	
+	Find David on youtube: https://www.youtube.com/watch?v=8XmxKPJDGU0
+	
+	His tutorials have inspired me to dive lower down in languages.
+	
+	This C source is a nearly direct port of his code, moving from OOP to functional
+	and adding some helper functions + a pseudo display device to hook into the bus.
+	
+	All cpu_<OPCODE> + cpu_<ADDRESSMODE> functions are basically unmodified, as well as the lookup array of INSTRUCTION structs.
+	
+*/
+
 #include "main.h"
 
 //declare the vars
